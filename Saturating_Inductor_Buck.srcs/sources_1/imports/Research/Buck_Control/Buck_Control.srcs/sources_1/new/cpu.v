333f5d5ef376cae3aa09e441772e43e88b3956cf
chrkelle <chrkelle@umich.edu>
2019-04-01 18:47:21 -0400

    done for day

--- Saturating_Inductor_Buck.srcs/sources_1/imports/Research/Buck_Control/Buck_Control.srcs/sources_1/new/cpu.v
+++ Saturating_Inductor_Buck.srcs/sources_1/imports/Research/Buck_Control/Buck_Control.srcs/sources_1/new/cpu.v
@@ -19,7 +19,7 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 module cpu
-# (localparam adc_delay = 1,on_time = 80,sw_off_delay= 200, sw_off_clear= 210, preset_delay = 4)
+# (localparam adc_delay = 1,on_time = 80,sw_off_delay= 140, sw_off_clear= 150, preset_delay = 4)
 (
 input wire clk,rst,
 input wire comp_edge, sat_flg,
