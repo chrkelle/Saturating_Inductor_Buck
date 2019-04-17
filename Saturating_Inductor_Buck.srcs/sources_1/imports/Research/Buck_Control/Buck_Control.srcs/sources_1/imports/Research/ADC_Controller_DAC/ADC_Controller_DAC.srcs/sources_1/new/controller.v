52e1ad1edb87d2742de1f36fdbc5e11f53ff12d8
chrkelle <chrkelle@umich.edu>
2019-04-17 13:17:38 -0400

    going back to 1 Mhz response

--- Saturating_Inductor_Buck.srcs/sources_1/imports/Research/Buck_Control/Buck_Control.srcs/sources_1/imports/Research/ADC_Controller_DAC/ADC_Controller_DAC.srcs/sources_1/new/controller.v
+++ Saturating_Inductor_Buck.srcs/sources_1/imports/Research/Buck_Control/Buck_Control.srcs/sources_1/imports/Research/ADC_Controller_DAC/ADC_Controller_DAC.srcs/sources_1/new/controller.v
@@ -78,7 +78,7 @@ module controller(clk, reset, step_up, ADC_done, ADC_in, i, control_done);
     reg signed [15:0] ADC;
     wire signed [15:0] ADC_error;
     wire signed [15:0] ADC_ref;
-    assign ADC_ref = step_up ? 250 : 188; //(1.8V)
+    assign ADC_ref = step_up ? 225 : 125; //(1.8V)
     //output reg signed [31:0] p_error;
     
     //s0 outputs    
