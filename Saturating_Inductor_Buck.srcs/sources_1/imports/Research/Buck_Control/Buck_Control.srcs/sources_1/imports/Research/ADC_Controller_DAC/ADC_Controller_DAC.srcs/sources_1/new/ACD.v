2281d97c23b6b53de4effc1ac3393c935d740e9d
Xiaofan Cui <richardcuixf@gmail.com>
2019-05-23 17:50:57 -0400

    DAC output test
    Assign all dac output bits to be 1

--- Saturating_Inductor_Buck.srcs/sources_1/imports/Research/Buck_Control/Buck_Control.srcs/sources_1/imports/Research/ADC_Controller_DAC/ADC_Controller_DAC.srcs/sources_1/new/ACD.v
+++ Saturating_Inductor_Buck.srcs/sources_1/imports/Research/Buck_Control/Buck_Control.srcs/sources_1/imports/Research/ADC_Controller_DAC/ADC_Controller_DAC.srcs/sources_1/new/ACD.v
@@ -94,13 +94,15 @@ module ACD(clk, reset, hi_muxsel, start, step_up, ctrl_start, dco_p, dco_n,
     
     always @(posedge clk) begin
         if(reset) begin
-            ctrl_2_dac <= 8192;
+            ctrl_2_dac <= 14'b11_1111_1111_1111;
         end
+        
         //else if(~start) begin
         //    ctrl_2_dac <= 8192;  
         //end
         else if(control_done) begin
-            ctrl_2_dac <= {1'b1,i_out[22:10]};
+            //ctrl_2_dac <= {1'b1,i_out[22:10]};
+            ctrl_2_dac <= 14'b11_1111_1111_1111;
         end
         
     end
